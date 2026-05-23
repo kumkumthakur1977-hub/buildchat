0a73af084bd0b99eaadbf41a9b9e7f773c259bfd
kumkumthakur1977-hub <kumkumthakur1977@gmail.com>
2026-05-23 12:47:08 +0530

    Delete app/(auth) directory

--- app/(auth)/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProfilePage() {
-    return (
-        <div style={{ padding: "40px", fontFamily: "sans-serif", color: "#fff" }}>
-            <h1>Profile</h1>
-        </div>
-    );
-}
